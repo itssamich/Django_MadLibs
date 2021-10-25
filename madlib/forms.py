9f9bd6ae4d5f3429820bf5133c474c1f8ed24652
Alex Klein <61808825+itssamich@users.noreply.github.com>
2021-10-25 19:46:29 -0400

    Fixed labels on form and form submission

--- madlib/forms.py
+++ madlib/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Madlib
 
-class libForm(forms.Form):
+class libForm(forms.ModelForm):
     class Meta:
+        model = Madlib
         fields = ['type1', 'type2', 'type3', 'type4', 'type5', 'type6', 'type7']
--- madlib/views.py
+++ madlib/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Madlib
 from django import forms
+from .forms import libForm
 
 # Create your views here.
 def index(request):
@@ -15,20 +16,32 @@ def index(request):
 
 def game(request, madlibId):
     lib = Madlib.objects.get(pk=madlibId)
+    class libForm(forms.ModelForm):
+        class Meta:
+            model = Madlib
+            fields = ['type1', 'type2', 'type3', 'type4', 'type5', 'type6', 'type7']
+            labels = {
+                'type1': lib.type1,
+                'type2': lib.type2,
+                'type3': lib.type3,
+                'type4': lib.type4,
+                'type5': lib.type5,
+                'type6': lib.type6,
+                'type7': lib.type7,
+            }
 
-    class libForm(forms.Form):
-        lib = Madlib.objects.get(pk=madlibId)
-        def __init__(self, *args, **kwargs):
-            for i in range(1, lib.blankCount+1):
-                field_name = eval('lib.type'+str(i))
-                self.fields[field_name] = forms.CharField(required=True)
-
-    
     form = libForm(request.POST or None)
+
+
     context = {
         'lib': lib,
         'form': form,
     }
     
+    if(form.is_valid()):
+        arr = []
+        for i in range(1, lib.blankCount+1):
+            arr.append(form.cleaned_data.get('type' + str(i)))
+        return render(request, 'madlib/game' +str(madlibId)+'.html', {'arr':arr})
 
     return render(request, 'madlib/game.html', context)
